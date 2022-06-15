63953c4efdd94c32c9e5ae8648db59decdaf3b91
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-06-15 23:12:28 +0530

    [Raj]Add: Create Connection with Address Book Database

---
+++ AddressBookADO/AddressBookADO/AddressBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace AddressBookADO
+{
+    public class AddressBook
+    {
+        public static string connection = @"Data Source = RAJVARDHAN; Initial Catalog = AddressBook_Service, Integrated Security=SSPI";
+        SqlConnection sqlConnection = new SqlConnection(connection);
+
+        public void SetConnection()
+        {
+            if (sqlConnection != null && sqlConnection.State.Equals(ConnectionState.Closed))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                }
+                catch (Exception)
+                {
+                    throw new CustomException(CustomException.ExceptionType.Connection_Failed, "Connection Failed");
+                }
+            }
+        }
+        public void Close()
+        {
+            if (sqlConnection != null && !sqlConnection.State.Equals(ConnectionState.Open))
+            {
+                try
+                {
+                    sqlConnection.Close();
+                }
+                catch (Exception)
+                {
+                    throw new CustomException(CustomException.ExceptionType.Connection_Failed, "Connection Failed");
+                }
+            }
+        }
+
+    }
+}
---
+++ AddressBookADO/AddressBookADO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AddressBookADO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBook.SetConnection();
+        }
+    }
+}
